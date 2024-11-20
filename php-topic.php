5dfac89a059bdaf7c610a02ceafda5fdc547e3e9
jamanul <jk.skb.25@gmail.com>
2024-11-20 16:07:06 +0600

    learned php

--- php-topic.php
+++ php-topic.php
@@ -4,19 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        body {
-            display: flex;
-            flex-direction: column;
-            height: 100vh;
-            justify-content: center;
-            align-items: center;
-            font-family: "sans-serif";
-        }
-    </style>
 </head>
 <body>
- <h1>recommended books</h1>
+    <!-- ep 9 -->
+<h1>recommended books</h1>
  <?php
  $books = [
     [
